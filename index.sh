d01bf0e544c72f207832a6446845996a9ce0a489
TalatiHardik <hardikhtalati@gmail.com>
2020-07-12 18:53:58 +0530

    UC2 Completed

--- index.sh
+++ index.sh
@@ -1,14 +1,32 @@
 
 #!/bin/bash -x
 
-random=$(( RANDOM%2 ))
+echo "Welcome to usecase 2"
+ishead=0
+headcount=0
+tailcount=0
+counter=1
+while [ $counter -lt 5 ]
+do
+        roll=$(( RANDOM%2 ))
 
 
-if [ $random -eq 0 ]
-then
-        coin=heads
-else
-        coin=tails
-fi
+        if [ $roll -eq $ishead ]
+        then
+                (( headcount++ ))
+                coin="Heads"
+        else
+                (( tailcount++ ))
+                coin="Tails"
+        fi
 
-echo "The coin is flipped and it is "$coin
+        echo "The coin is flipped and it is "$coin
+
+        (( counter++ ))
+
+done
+
+echo "The head is $headcount times"
+echo "The tail is $tailcount times"
+
+
