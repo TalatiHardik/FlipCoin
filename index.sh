3410ea3055b2d86a41b4a2368aea20b4f87d07c0
TalatiHardik <hardikhtalati@gmail.com>
2020-07-12 19:09:53 +0530

    UC4 completed

--- index.sh
+++ index.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash -x
 
-echo "Welcome to usecase 3"
+echo "Welcome to usecase 4"
 ishead=0
 headcount=0
 tailcount=0
@@ -26,13 +26,34 @@ do
 
 done
 
+if [ $headcount -eq $tailcount ]
+then
+	while [ $(( headcount-tailcount )) -gt 2 ]
+	do
+	        roll=$(( RANDOM%2 ))
+
+
+	        if [ $roll -eq $ishead ]
+	        then
+	                (( headcount++ ))
+	                coin="Heads"
+	        else
+	                (( tailcount++ ))
+	                coin="Tails"
+	        fi
+
+	        echo "The coin is flipped and it is "$coin
+
+	        (( counter++ ))
+
+	done
+echo "There was tie"
+fi
+
 if [ $headcount -gt $tailcount ]
 then
         echo "The head has falled $headcount times and it has won by "$(( $headcount-$tailcount )) "" difference
 
-elif [ $headcount -eq $tailcount ]
-then
-	echo "There is tie between head and tail and both falled "$headcount" times"
 else
         echo "The tail has falled $tailcount times and it has won by "$(( $tailcount-$headcount )) "" difference
 
