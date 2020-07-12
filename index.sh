eec00a3ec7215aedf2dacd1dacc36c00a8f38508
TalatiHardik <hardikhtalati@gmail.com>
2020-07-12 19:02:24 +0530

    UC3 completed

--- index.sh
+++ index.sh
@@ -1,12 +1,12 @@
 
 #!/bin/bash -x
 
-echo "Welcome to usecase 2"
+echo "Welcome to usecase 3"
 ishead=0
 headcount=0
 tailcount=0
 counter=1
-while [ $counter -lt 5 ]
+while [ $headcount -lt 21 ] && [ $tailcount -lt 21 ]
 do
         roll=$(( RANDOM%2 ))
 
@@ -26,7 +26,16 @@ do
 
 done
 
-echo "The head is $headcount times"
-echo "The tail is $tailcount times"
+if [ $headcount -gt $tailcount ]
+then
+        echo "The head has falled $headcount times and it has won by "$(( $headcount-$tailcount )) "" difference
+
+elif [ $headcount -eq $tailcount ]
+then
+	echo "There is tie between head and tail and both falled "$headcount" times"
+else
+        echo "The tail has falled $tailcount times and it has won by "$(( $tailcount-$headcount )) "" difference
+
+fi
 
 
